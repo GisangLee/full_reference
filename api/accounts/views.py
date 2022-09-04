779d3184e2695ea30d02c1867f443f797f517b7b
dude <masterkorea01@naver.com>
2022-09-04 16:07:41 +0900

    feat( retrieve user):
    Done

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -2,6 +2,7 @@
 
 
 # Django Packages
+import logging as log
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
@@ -14,6 +15,9 @@ from utils import mixins as util_mixins
 from utils.errors import Error
 from utils.success import Success
 from utils.swaggers.accounts import doc as swag_account_doc
+
+
+logger = log.getLogger("django.request")
 
 
 class UserViewSet(util_mixins.BaseViewSet):
@@ -48,7 +52,23 @@ class UserViewSet(util_mixins.BaseViewSet):
         users = self.get_queryset()
         users = list(users)
 
+        logger.debug(
+            {
+                "action": self.__class__.__name__,
+                "method": request.method,
+                "users": users,
+            }
+        )
+
         user_json = self.read_serializer_class(users, many=True)
+
+        logger.debug(
+            {
+                "action": self.__class__.__name__,
+                "method": request.method,
+                "user_json": user_json.data,
+            }
+        )
 
         return Response(
             Success.response(
@@ -66,6 +86,14 @@ class UserViewSet(util_mixins.BaseViewSet):
 
         new_user = self.serializer_class(data=request.data)
 
+        logger.debug(
+            {
+                "action": self.__class__.__name__,
+                "method": request.method,
+                "new_user": new_user,
+            }
+        )
+
         if new_user.is_valid():
             new_user.save()
             return Response(
@@ -75,6 +103,48 @@ class UserViewSet(util_mixins.BaseViewSet):
                 status=status.HTTP_201_CREATED,
             )
 
+        logger.debug(
+            {
+                "action": self.__class__.__name__,
+                "method": request.method,
+                "errors": new_user.errors,
+            }
+        )
+
         return Response(
             Error.error(new_user.errors), status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def retrieve(self, request, pk):
+
+        query = self.__detail_queryset(pk)
+
+        logger.debug(
+            {
+                "action": self.__class__.__name__,
+                "method": request.method,
+                "query": query,
+            }
+        )
+
+        if query is None:
+
+            return Response(
+                Error.error("사용자가 존재하지 않습니다."), status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user_json = self.read_serializer_class(query)
+
+        logger.debug(
+            {
+                "action": self.__class__.__name__,
+                "method": request.method,
+                "user_json": user_json,
+            }
+        )
+
+        return Response(
+            Success.response(
+                self.__class__.__name__, request.method, user_json.data, "200"
+            )
         )
